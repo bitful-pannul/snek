84a9457d6906f72352f8cfda5ef3b9bc28849802
bitful-pannul <bitful-pancreas@protonmail.com>
2024-07-29 14:24:57 +0300

    add game

--- snek/src/lib.rs
+++ snek/src/lib.rs
@@ -1,36 +1,19 @@
-use kinode_process_lib::{await_message, call_init, println, Address, Response};
+use kinode_process_lib::{await_message, call_init, http, println, Address};
 
 wit_bindgen::generate!({
     path: "target/wit",
     world: "process-v0",
 });
 
-fn handle_message(_our: &Address) -> anyhow::Result<()> {
-    let message = await_message()?;
-
-    if !message.is_request() {
-        return Err(anyhow::anyhow!("unexpected Response: {:?}", message));
-    }
-
-    let body: serde_json::Value = serde_json::from_slice(message.body())?;
-    println!("got {body:?}");
-    Response::new()
-        .body(serde_json::to_vec(&serde_json::json!("Ack")).unwrap())
-        .send()
-        .unwrap();
-    Ok(())
-}
-
 call_init!(init);
 fn init(our: Address) {
-    println!("begin");
+    println!("hello");
+
+    http::serve_index_html(&our, "ui", false, false, vec!["/"]).unwrap();
 
     loop {
-        match handle_message(&our) {
-            Ok(()) => {}
-            Err(e) => {
-                println!("error: {:?}", e);
-            }
+        match await_message() {
+            _ => {}
         };
     }
 }
