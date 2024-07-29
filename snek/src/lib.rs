48224dc06bcf3cf836eb30869fd7ca956ced7a98
bitful-pannul <bitful-pancreas@protonmail.com>
2024-07-29 14:47:59 +0300

    add icon

--- snek/src/lib.rs
+++ snek/src/lib.rs
@@ -1,4 +1,5 @@
-use kinode_process_lib::{await_message, call_init, http, println, Address};
+use kinode_process_lib::{await_message, call_init, homepage, http, println, Address};
+static SNEK_SVG: &str = include_str!("../../pkg/ui/snek.svg");
 
 wit_bindgen::generate!({
     path: "target/wit",
@@ -10,7 +11,7 @@ fn init(our: Address) {
     println!("hello");
 
     http::serve_index_html(&our, "ui", false, false, vec!["/"]).unwrap();
-
+    homepage::add_to_homepage("snek", Some(SNEK_SVG), Some("/"), None);
     loop {
         match await_message() {
             _ => {}
